e9583c739b247df758e10511869d141c003aa7b1
Thiago Oliveira <thiago.about@gmail.com>
2017-12-09 17:23:56 -0200

    add user auth create and update

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,7 +2,40 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user_params = %{
+      token: auth.credentials.token,
+      name: auth.info.name,
+      email: auth.info.email,
+      provider: provider
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error siging in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
